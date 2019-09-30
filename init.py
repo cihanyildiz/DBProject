8c6b1c433134192e9240f817eae7fcdaadb0c9f9
cihanyildiz <yildizzcihann@gmail.com>
2019-09-30 13:55:52 +0300

    first commit

--- init.py
+++ init.py
@@ -1,2 +1,11 @@
 import sys
 from postgres import PostgresSql
+
+x = PostgresSql("gis", "postgres", "60", "localhost", "5432")
+cursor = x.get_connection().cursor()
+cursor.execute("SELECT *FROM DENEME_TABLE")
+mobile_records = cursor.fetchall()
+for row in mobile_records:
+    print("", row[0],)
+    print("", row[1])
+    print("", row[2], "\n")
